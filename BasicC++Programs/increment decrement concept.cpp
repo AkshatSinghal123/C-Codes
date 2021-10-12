339a6031b17bdeb20d79553c9e8d02f0d8b58927
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-12 21:40:30 +0530

    add increment and decrement concept

---
+++ BasicC++Programs/increment decrement concept.cpp
@@ -0,0 +1,26 @@
+/*Precedence operator concept
+int x=2;
+x++;
+cout<<x;//2
+------------
+int x=2;
+++x;
+cout<<x;//3*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int i=5,j;
+ j=i++;
+ cout<<j<<" "<<i<<endl;// 5  6
+int k=5,l;
+ l=++k;
+ cout<<l<<" "<<k<<endl;// 6   6
+int a=5,b;
+ b=2*++a + 2*a++;
+ cout<<b<<" "<<a<<endl;;// 26  7
+int c=5,d;
+ d=2*c++ + 2*c++;
+ cout<<d<<" "<<c<<endl;// 22  7
+return 0;
+}
