bb8e6d1a46d9e6cfd8ebad90e75e7f804af159f8
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-12 20:54:16 +0530

    remove

--- Finding sum of N natural nos.c
+++
@@ -1,9 +0,0 @@
-#include<iostream.h>
-using namespace std;
-int main(){
- int sum,n;
- cout<<"Enter the limit where you want to find the sum";
- cin>>n;
- sum=(n*(n+1))/2
- cout<<"Sum of"<<n<<"natural nos is "<<sum;
-}
