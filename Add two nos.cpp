4b574c947cf1143bfeaa722f44fae5742e39cda0
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-12 20:48:08 +0530

    add basic programs of c++

---
+++ Add two nos.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b,c;
+ cout<<"Enter two nos";
+ cin>>a>>b;
+ c=a+b;
+ cout<<"Addition is "<<c;
+}
---
+++ Calculating Area of triangle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int b,h,area;
+ cout<<"Enter the base and height of the triangle ";
+ cin>>b>>h;
+ area=(b*h)/2;
+ cout<<"Area of Triangle is "<<area;
+}
