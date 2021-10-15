4d9fc400fd5f294360a1abdeca5d0ebfda41d611
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-15 09:16:22 +0530

    add Finding the max of two numbers program

---
+++ Conditionals/Finding Max of two numbers.cpp
@@ -0,0 +1,15 @@
+//Conditionals Finding maximum of two numbers
+#include<iostream>
+using namespace std;
+int main(){
+ int num1,num2;
+ cout<<"Enter the two numbers for which you have to find the comparison";
+ cin>>num1>>num2;
+ if(num1>num2){
+    cout<<"The greater among the two is "<<num1;
+ }
+ else{
+    cout<<"The greater among the two is "<<num2;
+ }
+ return 0;
+}
