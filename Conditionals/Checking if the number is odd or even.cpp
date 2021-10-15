61ddb9ca9054ff753fe4f62c9002c0066c9c8974
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-15 09:31:19 +0530

    Checking if the number is odd or even

---
+++ Conditionals/Checking if the number is odd or even.cpp
@@ -0,0 +1,14 @@
+//Checking the enetered number is odd or even
+#include<iostream>
+using namespace std;
+int main(){
+ int number;
+ cout<<"Enter the number";
+ cin>>number;
+ if(number%2==0){
+    cout<<"The entered number "<<number<<" is even number ";
+ }
+ else{
+    cout<<"The entered number "<<number<<" is odd number ";
+ }
+}
