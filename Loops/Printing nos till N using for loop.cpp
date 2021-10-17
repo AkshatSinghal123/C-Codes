e6421c923d1e1d39ef777b2b69fd7e701389365d
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-17 09:56:27 +0530

    Printing nos till N using for loop

---
+++ Loops/Printing nos till N using for loop.cpp
@@ -0,0 +1,12 @@
+//Printing nos till N using for loop
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cout<<"Enter the range uptil which you want to print the nos";
+ cin>>n;
+ for(int i=1;i<=n;i++){
+  cout<<i<<" ";
+ }
+ return 0;
+}
