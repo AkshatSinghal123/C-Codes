a2599635847b31b64c86c3ce08fd87d3fafd9522
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-10-17 10:00:19 +0530

    infinite loop

---
+++ Loops/infinite loop.cpp
@@ -0,0 +1,12 @@
+//infinite loop
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cout<<"Enter the range uptil which you want to print the nos";
+ cin>>n;
+ for(;;){
+  cout<<"Hello";
+ }
+ return 0;
+}
