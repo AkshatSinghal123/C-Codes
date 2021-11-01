b08e0e046cf293a7d8a9b7361398a0dad393e4b8
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2021-11-01 14:39:38 +0530

    Scoping rules.cpp

---
+++ Functions/Scoping rules.cpp
@@ -0,0 +1,13 @@
+//Scoping rules
+#include<iostream>
+using namespace std;
+int x=10;
+int main(){
+ int x=20;
+ {
+   int x=30;
+   cout<<x<<endl;
+ }
+ cout<<x<<endl;
+ cout<<::x<<endl;//Global var accessed using "::" scope resolution operator
+}
